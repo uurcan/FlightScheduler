fb3432e8919de089669ceb68f143168e1a1d3a96
uurcan <ugurcancelebi1@gmail.com>
2021-05-31 00:21:41 +0300

    Fragment structure taken its heritance from base class

--- app/src/main/java/com/java/flightscheduler/utils/BindingUtils.kt
+++ app/src/main/java/com/java/flightscheduler/utils/BindingUtils.kt
@@ -1,8 +1,23 @@
 package com.java.flightscheduler.utils
 
 import android.view.View
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.java.flightscheduler.data.constants.AppConstants
+
+@BindingAdapter("isRefreshing")
+fun SwipeRefreshLayout.customRefreshing(refreshing: Boolean?){
+    isRefreshing = refreshing == true
+}
+
+@BindingAdapter(value = ["isLoading"])
+fun ContentLoadingProgressBar.show(isLoading : Boolean?){
+    if (isLoading == true)
+        show()
+    else
+        hide()
+}
 
 @BindingAdapter("clickSafe")
 fun View.setClickSafe(listener : View.OnClickListener?){
