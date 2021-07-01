4996463f8b867586486fc2ccf1d32054313b085f
ucelebi <ugurcancelebi1@gmail.com>
2021-07-01 23:25:52 +0300

    Client Secrets are being stored in properties file..

--- app/src/main/java/com/java/flightscheduler/data/constants/HttpConstants.kt
+++ app/src/main/java/com/java/flightscheduler/data/constants/HttpConstants.kt
@@ -1,11 +1,13 @@
 package com.java.flightscheduler.data.constants
 
+import com.java.flightscheduler.BuildConfig
+
 object HttpConstants {
     const val BASE_PATH = "v1/"
     const val GET = "GET"
     const val POST = "POST"
     const val DELETE = "DELETE"
     const val AUTH = "Authorization"
-    const val clientId = "g0Bxb6Aar7qN0SNg22fGfGZJG0Uy1YWz"
-    const val clientSecret = "HAWQf0DsgPedZLGo"
+    const val clientId = BuildConfig.API_KEY
+    const val clientSecret = BuildConfig.API_SECRET
 }
