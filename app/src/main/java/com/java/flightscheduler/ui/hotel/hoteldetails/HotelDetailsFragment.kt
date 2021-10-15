e8244a7b868d18a11c47968a8289336643d51043
uurcan <ugurcancelebi1@gmail.com>
2021-10-16 01:50:12 +0300

    Hotel - UI - Part7 - Permissions in hotel details

--- app/src/main/java/com/java/flightscheduler/ui/hotel/hoteldetails/HotelDetailsFragment.kt
+++ app/src/main/java/com/java/flightscheduler/ui/hotel/hoteldetails/HotelDetailsFragment.kt
@@ -1,28 +1,33 @@
 package com.java.flightscheduler.ui.hotel.hoteldetails
 
-import android.graphics.drawable.Drawable
-import android.os.Build
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.java.flightscheduler.BR
 import com.java.flightscheduler.R
-import com.java.flightscheduler.data.model.hotel.base.HotelOffer
+import com.java.flightscheduler.data.constants.AppConstants.REQUEST_CODE_CALL_PERMISSION
 import com.java.flightscheduler.databinding.FragmentHotelDetailBinding
+import com.java.flightscheduler.utils.PermissionUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
 @AndroidEntryPoint
-class HotelDetailsFragment : Fragment() {
+class HotelDetailsFragment : Fragment(),OnMapReadyCallback,EasyPermissions.PermissionCallbacks,View.OnClickListener {
     private val args by navArgs<HotelDetailsFragmentArgs>()
     private val hotelDetailsViewModel : HotelDetailsViewModel by viewModels()
     private lateinit var binding : FragmentHotelDetailBinding
@@ -43,12 +48,75 @@ class HotelDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializeResults(args.hotel)
-        binding.setVariable(BR.hotelDetail,args.hotel)
+        initializeViews()
+        initializeVariables()
+        initializeMap()
     }
 
-    private fun initializeResults(hotel: HotelOffer) {
-        Log.d("AMATEST", hotel.hotel?.media?.get(0)?.uri.toString())
-        //binding..text = hotel.offers?.get(0)?.checkInDate.toString()
+    private fun initializeViews() {
+        binding.layoutHotelDetailContact.setOnClickListener(this)
+    }
+
+    private fun initializeVariables() {
+        binding.setVariable(BR.hotelDetail, args.hotel)
+    }
+
+    private fun initializeMap() {
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.layout_hotel_detail_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        hotelDetailsViewModel.getCoordinatesLiveData(
+            args.hotel.hotel!!.latitude, args.hotel.hotel!!.longitude
+        )?.observe(viewLifecycleOwner, { coordinates ->
+            val location = LatLng(coordinates.first, coordinates.second)
+            map.addMarker(MarkerOptions().position(location))
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15F))
+        })
+    }
+
+    private fun requestPermissions() {
+        if (PermissionUtility.hasPhoneDialPermission(requireContext())) {
+            startDialActivity()
+        }
+        EasyPermissions.requestPermissions(
+            this,
+            getString(R.string.text_permission_required),
+            REQUEST_CODE_CALL_PERMISSION,
+            android.Manifest.permission.CALL_PHONE
+        )
+    }
+
+    private fun startDialActivity() {
+        val number = args.hotel.hotel?.contact?.phone.toString()
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$number")
+        startActivity(intent)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            binding.layoutHotelDetailContact.id -> requestPermissions()
+        }
     }
 }
--- app/src/main/java/com/java/flightscheduler/ui/hotel/hoteldetails/HotelDetailsViewModel.kt
+++ app/src/main/java/com/java/flightscheduler/ui/hotel/hoteldetails/HotelDetailsViewModel.kt
@@ -1,10 +1,18 @@
 package com.java.flightscheduler.ui.hotel.hoteldetails
 
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HotelDetailsViewModel @Inject constructor() : ViewModel() {
+    private val coordinatesLiveData : MutableLiveData<Pair<Double,Double>>? = MutableLiveData()
 
+
+    fun getCoordinatesLiveData(latitude : Double, longitude : Double) : MutableLiveData<Pair<Double,Double>>? {
+        coordinatesLiveData?.value = Pair(latitude,longitude)
+        return coordinatesLiveData
+    }
 }
