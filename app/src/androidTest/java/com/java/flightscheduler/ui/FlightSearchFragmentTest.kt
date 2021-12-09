b672b748ea1e5ff1c6a763b21bfb563ecc3a6328
uurcan <ugurcancelebi1@gmail.com>
2021-12-09 21:45:06 +0300

    Test - Instrumentation UI - Part6 - Field validations for Search screen

--- app/src/androidTest/java/com/java/flightscheduler/ui/FlightSearchFragmentTest.kt
+++ app/src/androidTest/java/com/java/flightscheduler/ui/FlightSearchFragmentTest.kt
@@ -1,27 +1,31 @@
 package com.java.flightscheduler.ui
 
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.filters.MediumTest
-import com.java.flightscheduler.launchFragmentInHiltContainer
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.MediumTest
 import com.java.flightscheduler.R
 import com.java.flightscheduler.data.model.flight.Airport
 import com.java.flightscheduler.data.model.flight.FlightSearch
+import com.java.flightscheduler.launchFragmentInHiltContainer
 import com.java.flightscheduler.ui.flight.flightsearch.FlightSearchFragment
 import com.java.flightscheduler.ui.flight.flightsearch.FlightSearchFragmentDirections
 import com.java.flightscheduler.ui.home.HomeFragment
 import com.java.flightscheduler.ui.home.HomeFragmentDirections
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+
 
 @MediumTest
 @HiltAndroidTest
@@ -30,15 +34,18 @@ class FlightSearchFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    private lateinit var  navController : NavController
+
+    private lateinit var fragmentActivity : FragmentActivity
+
     @Before
     fun setup() {
         hiltRule.inject()
+        navController = mock(NavController::class.java)
     }
 
     @Test
     fun clickHomeFragmentOffersButton_navigateToFlightSearchFragment() {
-        val navController = mock(NavController::class.java)
-
         launchFragmentInHiltContainer<HomeFragment> {
             Navigation.setViewNavController(requireView(), navController)
         }
@@ -50,9 +57,28 @@ class FlightSearchFragmentTest {
     }
 
     @Test
-    fun clickFlightSearchFragmentSubmitButton_navigateToFlightResultsFragment() {
-        val navController = mock(NavController::class.java)
+    fun flightSearchOriginDestinationAutoComplete_fillAirportResultsBySelectingTheListItem() {
+        launchFragmentInHiltContainer<FlightSearchFragment> {
+            Navigation.setViewNavController(requireView(), navController)
+            fragmentActivity = activity!!
+        }
 
+        onView(withId(R.id.edt_flight_search_origin))
+            .perform(clearText(), typeText("Istanbul"), closeSoftKeyboard())
+
+        onView(withText("IST"))
+            .inRoot(withDecorView(not(fragmentActivity.window.decorView)))
+            .perform(click())
+
+        onView(withId(R.id.edt_flight_search_destination))
+            .perform(clearText(), typeText("Izmir"), closeSoftKeyboard())
+
+        onView(withText("ADB"))
+            .inRoot(withDecorView(not(fragmentActivity.window.decorView)))
+            .perform(click())
+    }
+    @Test
+    fun clickFlightSearchFragmentSubmitButton_navigateToFlightResultsFragment() {
         val flightSearch = FlightSearch(
             origin = Airport(
                 NAME = "Sabiha Gokcen", IATA = "SAW", CITY = "ISTANBUL"
@@ -70,10 +96,10 @@ class FlightSearchFragmentTest {
         launchFragmentInHiltContainer<FlightSearchFragment> {
             Navigation.setViewNavController(requireView(), navController)
         }
-        onView(withId(R.id.btn_flight_search_flights)).perform(click())
 
+        onView(withId(R.id.btn_flight_search_flights)).perform(click())
         verify(navController).navigate(
-            FlightSearchFragmentDirections.actionNavFlightSearchToNavFlightResults(flightSearch).actionId
+            FlightSearchFragmentDirections.actionNavFlightSearchToNavFlightResults(flightSearch)
         )
     }
 }
