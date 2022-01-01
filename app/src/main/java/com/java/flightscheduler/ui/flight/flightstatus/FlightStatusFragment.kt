c314cc0a8d53a2cc572935913c306e0219f6901c
uurcan <ugurcancelebi1@gmail.com>
2022-01-01 22:48:55 +0300

    Flight - Status - Part1 - First Display with compose

--- app/src/main/java/com/java/flightscheduler/ui/flight/flightstatus/FlightStatusFragment.kt
+++ app/src/main/java/com/java/flightscheduler/ui/flight/flightstatus/FlightStatusFragment.kt
@@ -4,31 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.java.flightscheduler.R
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_flight_status.*
 
 @AndroidEntryPoint
 class FlightStatusFragment : Fragment() {
-    private lateinit var flightStatusViewModel: FlightStatusViewModel
-
+    private val flightStatusViewModel : FlightStatusViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_flight_status, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        flightStatusViewModel = ViewModelProvider(this).get(FlightStatusViewModel::class.java)
-        flightStatusViewModel.getFlightStatusLiveData()?.observe(viewLifecycleOwner, {
-                flightStatus ->
-            if (flightStatus != null)
-                text_flight_status.text = flightStatus[0].flightDesignator?.carrierCode.toString()
-        })
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                FlightStatusScreen(flightStatusViewModel)
+            }
+        }
     }
 }
